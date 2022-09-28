7889aa7eaffaa23663e7b11800e3af0bdedc7d75
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-28 23:28:37 +0530

    [Pratu]ADDED: Ability to Create a Linked List by Adding

--- LinkedList-Problem/LinkListOpertion.cs
+++ LinkedList-Problem/LinkListOpertion.cs
@@ -28,25 +28,30 @@ namespace LinkedList_Problem
                 head = newNode;
             }
         }
+
+        public void Add(int value)
+        {
+            Node newNode = new Node();
+            newNode.data = value;
+            newNode.next = null;
+            Node temp = head;
+            head = newNode;
+            newNode.next = temp;
+        }
+
         public void display()
         {
-            if (head == null)
+            Node temp = head;
+            while (temp.next != null)
             {
-                Console.WriteLine("Linked List is empty");
-            }
-            else
-            {
-                Node temp = head;
-
-                while (temp.next != null)
-                {
-                    Console.WriteLine(temp.data);
-                    temp = temp.next;
-                }
                 Console.WriteLine(temp.data);
+                temp = temp.next;
             }
+            Console.WriteLine(temp.data);
         }
     }
 }
 
-    
+
+
+
