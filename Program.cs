3fea9ca13f73a656786eda25c34508a2011d950b
pratiksha-bhange <pratikshabhange101@gmail.com>
2022-09-28 19:30:10 +0530

    [Pratu]ADDED: Welcome to Linked List Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList_Problem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Data structure program using Linked List");
+        }
+    }
+}
